89042de7a8c2820fc060fe08b154d049a1f2c954
arun <arun36824@gmail.com>
2023-01-07 17:19:06 +0530

    git cards created

--- JS/Github.js
+++ JS/Github.js
@@ -1,19 +1,52 @@
-var username = document.getElementById("userID");
-
-document.getElementById("userID").addEventListener("click", function () {
-  form.submit();
-});
-
-console.log(username);
 async function getRepos(username) {
-	const response = await fetch(`https://api.github.com/users/${username}/repos`);
-	const data = await response.json();
-	return data;
-  }
-  
-  async function main() {
-	const repos = await getRepos("octocat");
-	console.log(repos);
-  }
-  
-  main();
+  const response = await fetch(`https://api.github.com/users/${username}/repos`);
+  const data = await response.json();
+  return data;
+}
+
+async function main() {
+  const repos = await getRepos("akycdi");
+  console.log(repos);
+
+  var cardContainer = document.getElementById('cardcontainer');
+
+
+  repos.forEach((item) => {
+    var columncard = document.createElement('div');
+    var card = document.createElement('div')
+    var card_inner = document.createElement("div");
+    var button = document.createElement('button');
+    var h2Name = document.createElement('h6');
+    var paragraph = document.createElement('p');
+    card_inner.className = "card-body"
+    card.className = "card";
+    columncard.className = "col-md-4 col-sm-6 col-lg-3";
+    paragraph.appendChild(document.createTextNode(item.description));
+    h2Name.appendChild(document.createTextNode(item.name));
+
+    button.type = 'button';
+    button.innerHTML = item.name;
+    button.className = 'btn btn-primary btn-sm';
+    button.onclick = function () {
+
+    };
+
+    card_inner.appendChild(h2Name);
+    card_inner.appendChild(paragraph);
+    card_inner.appendChild(button);
+    card.appendChild(card_inner);
+    columncard.appendChild(card);
+    cardContainer.appendChild(columncard);
+
+  });
+
+  var i = 0;
+  repos.forEach(element => {
+    console.log(i++);
+    console.log(element.name);
+    console.log(element.url);
+
+  });
+}
+
+main();
